9b1e88318b7cd3468ce79811c1017644869e3067
rohinvaidya <rohinvaidya@outlook.com>
2025-06-02 10:52:09 -0400

    Implemented basic save-to-file feature

--- seo_fetcher.py
+++ seo_fetcher.py
@@ -12,4 +12,17 @@ def returnMetricsForKeyword(keyword):
         "keyword_difficulty": 0.75,
         "avg_cpc": 1.50,
     }
+    saveMetricsToFile(metrics)
     return metrics
+
+def saveMetricsToFile(metrics, filename='metrics.json'):
+    """
+    Saves the provided metrics to a JSON file.
+    
+    Args:
+        metrics (dict): The metrics to save.
+        filename (str): The name of the file to save the metrics to.
+    """
+    import json
+    with open(filename, 'w') as f:
+        json.dump(metrics, f, indent=4)
